b02eaf73380c533511e3be27ab9a4c0ed36712a0
Sambit <Sambit@HYL-777520.local>
2020-06-17 01:58:02 +0530

    saving untested work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { SpeechRecognizer, SpeechEvents } from './speech.service';
 @Component({
-  selector: 'app-root',
+  selector: 'speecher-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   title = 'speech-showcase';
-  $: any;
-  result: string;
-  started = false;
-  state = '';
-  constructor(private recognizer: SpeechRecognizer) {
-    console.log('New App component created');
+  constructor() {
   }
   ngOnInit(){
-  }
-  start() {
-    if ( this.started ){
-      this.stop();
-      return;
-    }
-
-    this.recognizer.start().subscribe(( { result, event, error }) => {
-      if ( event === SpeechEvents.didStartListening ) {
-        this.started = true;
-      }
-      if ( event === SpeechEvents.didStopListening ) {
-        this.started = false;
-      }
-      if (event === SpeechEvents.didReceiveResult ) {
-        this.result += result.speech;
-      }
-    });
-  }
-
-  private stop() {
-    this.recognizer.stop();
-    this.state = 'Idle';
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { HomeComponent } from '@components/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
